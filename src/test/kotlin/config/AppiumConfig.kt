15674e5135bb4acef4691aa12822a3489fded339
Tsoi Anatoli <mamayatester@outlook.com>
2023-08-08 18:36:01 +0200

    first commit. Набросал простую конфигурацию с плагином selenide-appium

---
+++ src/test/kotlin/config/AppiumConfig.kt
@@ -0,0 +1,21 @@
+package config
+
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.remote.MobileCapabilityType
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
+class AppiumConfig {
+    val driver: AndroidDriver
+
+    init {
+        val capabilities = DesiredCapabilities()
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554")
+        capabilities.setCapability("autoAcceptAlerts", true)
+        capabilities.setCapability("noReset", "true")
+        capabilities.setCapability("appPackage", EnvironmentConfig.APP_PACKAGE)
+        capabilities.setCapability("appActivity", EnvironmentConfig.APP_ACTIVITY)
+        driver = AndroidDriver(URL("${EnvironmentConfig.TEST_HOST}/wd/hub"), capabilities)
+    }
+}
---
+++ src/test/kotlin/config/EnvoronmentConfig.kt
@@ -0,0 +1,7 @@
+package config
+
+object EnvironmentConfig {
+    const val APP_PACKAGE = "com.rimidalv.dictaphone"
+    const val APP_ACTIVITY = "com.rimidalv.dictaphone.feature.main.MainActivity"
+    const val TEST_HOST = "http://127.0.0.1:4723"
+}
