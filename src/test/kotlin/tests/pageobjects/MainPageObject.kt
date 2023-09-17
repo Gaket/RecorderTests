01fdc8a5af19afc1fb1dacf9f7b0999c281fe2be
Umar116 <Ilnur16REGGY@gmail.com>
2023-09-17 13:33:10 +0700

    к методу clickRecordButton добавил проверку отображения заголовка страницы записи после клика на запись

--- src/test/kotlin/tests/pageobjects/MainPageObject.kt
+++ src/test/kotlin/tests/pageobjects/MainPageObject.kt
@@ -1,13 +1,15 @@
 package tests.pageobjects
 
+import com.codeborne.selenide.Condition
 import com.codeborne.selenide.appium.SelenideAppium.`$`
 import io.appium.java_client.AppiumBy
 
 class MainPageObject {
     private val recordButton = `$`(AppiumBy.id("com.rimidalv.dictaphone:id/recPauseBtn"))
 
-
     fun clickRecordButton() {
         recordButton.click()
+        val recordingPageTitle = `$`(AppiumBy.id("com.rimidalv.dictaphone:id/textView2"))
+        recordingPageTitle.shouldBe(Condition.visible)
     }
 }
