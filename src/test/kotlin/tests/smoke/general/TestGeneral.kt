19a6437036f8583cc832e7f8acd8742103f5676e
Umar116 <Ilnur16REGGY@gmail.com>
2023-09-17 13:43:16 +0700

    1) Удалил тест который просто запускает приложение 2) Написал проверку на отображение записи в списке записей после сохранения

--- src/test/kotlin/tests/smoke/general/TestGeneral.kt
+++ src/test/kotlin/tests/smoke/general/TestGeneral.kt
@@ -1,18 +1,31 @@
 package tests.smoke.general
 
+import com.codeborne.selenide.Condition
+import io.github.serpro69.kfaker.Faker
 import org.junit.jupiter.api.Test
 import tests.BaseTest
+import tests.pageobjects.ListOfRecordingsPageObject
 import tests.pageobjects.MainPageObject
 import tests.pageobjects.RecordingPageObject
+import tests.pageobjects.RecordingSaveWindowPageObject
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 
 class TestTimerWhenRecordingIsOn : BaseTest() {
+
     @Test
-    fun test() {
-        val waitTime = Random.nextInt(5, 76)
+    fun recordingIsDisplayedInTheListOfRecordsListAfterSaving(){
+        val fake = Faker()
+        val fileName = fake.random.randomString(1, 10)
         val mainPageObject = MainPageObject()
+        val recordingSaveWindow = RecordingSaveWindowPageObject()
+        val recorderPageObject = RecordingPageObject()
+        val listOfRecordings = ListOfRecordingsPageObject()
+        val waitTime = Random.nextInt(5, 10)
         mainPageObject.clickRecordButton()
         RecordingPageObject().checkUiTimeInProgress(waitTime.seconds)
+        recorderPageObject.clickStopRecording()
+        recordingSaveWindow.saveRecordingFile(fileName)
+        listOfRecordings.getFileNameTile().should(Condition.have(Condition.text(fileName)))
     }
 }
