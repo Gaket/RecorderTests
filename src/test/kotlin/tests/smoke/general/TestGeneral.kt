95e8a2e7198375a436ad37a311e201de27446c00
Umar116 <Ilnur16REGGY@gmail.com>
2023-09-26 12:00:38 +0700

    Сделал вызов методов статичными

--- src/test/kotlin/tests/smoke/general/TestGeneral.kt
+++ src/test/kotlin/tests/smoke/general/TestGeneral.kt
@@ -2,6 +2,7 @@ package tests.smoke.general
 
 import com.codeborne.selenide.Condition
 import io.github.serpro69.kfaker.Faker
+import io.qase.api.annotation.QaseTitle
 import org.junit.jupiter.api.Test
 import tests.BaseTest
 import tests.pageobjects.ListOfRecordingsPageObject
@@ -12,20 +13,19 @@ import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 
 class TestTimerWhenRecordingIsOn : BaseTest() {
-
+    @QaseTitle("REC-1")
     @Test
     fun recordingIsDisplayedInTheListOfRecordsListAfterSaving(){
         val fake = Faker()
         val fileName = fake.random.randomString(1, 10)
         val mainPageObject = MainPageObject()
-        val recordingSaveWindow = RecordingSaveWindowPageObject()
-        val recorderPageObject = RecordingPageObject()
-        val listOfRecordings = ListOfRecordingsPageObject()
         val waitTime = Random.nextInt(5, 10)
+
         mainPageObject.clickRecordButton()
         RecordingPageObject().checkUiTimeInProgress(waitTime.seconds)
-        recorderPageObject.clickStopRecording()
-        recordingSaveWindow.saveRecordingFile(fileName)
-        listOfRecordings.getFileNameTile().should(Condition.have(Condition.text(fileName)))
+        RecordingPageObject().clickStopRecording()
+        RecordingPageObject().saveRecordingWindowTitle().shouldBe(Condition.visible)
+        RecordingSaveWindowPageObject().saveRecordingFile(fileName)
+        ListOfRecordingsPageObject().fileNameTitle().should(Condition.have(Condition.text(fileName)))
     }
 }
