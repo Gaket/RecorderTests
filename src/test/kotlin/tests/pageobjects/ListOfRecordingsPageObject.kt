3ba6045dd47358f4b728a38674431f30a9f70bc8
Umar116 <Ilnur16REGGY@gmail.com>
2023-09-26 11:58:59 +0700

    Удалил лишнюю переменную fileName и сделал ее как метод

--- src/test/kotlin/tests/pageobjects/ListOfRecordingsPageObject.kt
+++ src/test/kotlin/tests/pageobjects/ListOfRecordingsPageObject.kt
@@ -6,10 +6,20 @@ import io.appium.java_client.AppiumBy
 
 class ListOfRecordingsPageObject {
 
-    private val fileName = `$`(AppiumBy.id("com.rimidalv.dictaphone:id/row_file_name"))
+    private val seekBar = `$`(AppiumBy.id("com.rimidalv.dictaphone:id/seekBar"))
+    private val playRecordButton = `$`(AppiumBy.id("com.rimidalv.dictaphone:id/row_img_play"))
 
-    fun getFileNameTile(): SelenideAppiumElement {
-        return fileName
+    fun fileNameTitle() :SelenideAppiumElement{
+        return `$`(AppiumBy.id("com.rimidalv.dictaphone:id/row_file_name"))
+    }
+
+    fun getSeekBar(): SelenideAppiumElement{
+        return seekBar
+    }
+
+
+    fun clickPlayRecordButton(){
+        playRecordButton.click()
     }
 
 
